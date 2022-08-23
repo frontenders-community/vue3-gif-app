ce66fe2cd22a94f96cd7909164055ebf6e940055
olga-demina <olga.demina1410@gmail.com>
2022-08-23 16:09:41 +0200

    Project scaffolding

---
+++ README.md
@@ -0,0 +1,2 @@
+### This is a simple learning project to start using composition api of vue3
+### We will use https://tenor.com/gifapi for searching gifs by keywords.
---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+  <header>
+    Header
+  </header>
+
+  <main>
+    Main
+  </main>
+</template>
+
+<style scoped>
+
+</style>
